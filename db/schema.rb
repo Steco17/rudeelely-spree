ee5ab13040e90811e334b67d0dd6567e02caf1b3
steco17 <moyo.stephane@gmail.com>
2019-08-11 12:07:34 +0100

    static Pages

--- db/schema.rb
+++ db/schema.rb
@@ -332,6 +332,35 @@ ActiveRecord::Schema.define(version: 2019_06_17_162461) do
     t.index ["promotion_code_id"], name: "index_spree_orders_promotions_on_promotion_code_id"
   end
 
+  create_table "spree_pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean "show_in_header", default: false, null: false
+    t.boolean "show_in_footer", default: false, null: false
+    t.string "foreign_link"
+    t.integer "position", default: 1, null: false
+    t.boolean "visible", default: true
+    t.string "meta_keywords"
+    t.string "meta_description"
+    t.string "layout"
+    t.boolean "show_in_sidebar", default: false, null: false
+    t.string "meta_title"
+    t.boolean "render_layout_as_partial", default: false
+    t.index ["slug"], name: "index_spree_pages_on_slug"
+  end
+
+  create_table "spree_pages_stores", id: false, force: :cascade do |t|
+    t.integer "store_id"
+    t.integer "page_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["page_id"], name: "index_spree_pages_stores_on_page_id"
+    t.index ["store_id"], name: "index_spree_pages_stores_on_store_id"
+  end
+
   create_table "spree_payment_capture_events", force: :cascade do |t|
     t.decimal "amount", precision: 10, scale: 2, default: "0.0"
     t.integer "payment_id"
